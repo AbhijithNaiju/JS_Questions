268d0d9f86b04fd32b86084dc23af64bcb0d67de
AbhijithNaiju <abhijithnaiju@gmail.com>
2024-10-09 18:23:50 +0530

    Commit 09-10-2024

--- JS/js_qn.js
+++ JS/js_qn.js
@@ -5,5 +5,7 @@ function hello_world()
 
 function add_2_num()
 {
-	
+	num_1=document.getElementById("num_1").value;
+	num_2=document.getElementById("num_2").value;
+	document.getElementById("ans_2").innerHTML = Number(num_1)+Number(num_2);
 }
